70251609e2b1f1d8753765c70bd33c9a0ff67a86
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-11 10:13:18 +0530

    Diplaying Bars in Bar Chart

--- Data Visualization Certif/3. Projects/1. Visualize Data with a Bar Chart/BarGraph.js
+++ Data Visualization Certif/3. Projects/1. Visualize Data with a Bar Chart/BarGraph.js
@@ -1,8 +1,36 @@
-const dataset = [12, 31, 22, 34, 15];
+const dataset = [12 , 31 , 22 , 17 , 25 , 18 , 29 , 14 , 9];
+const w = 500;
+const h = 100;
+const padding = 60 ;
+const xScale = d3.scaleLinear()
+                 .domain([ 0 , d3.max(dataset, (d) => d[ 0 ])])
+                 .range([padding, w - padding]);
+const yScale = d3.scaleLinear()
+                 .domain([ 0 , d3.max(dataset, (d) => d[ 1 ])])
+                 .range([h - padding, padding]);
+                 
+const xAxis = d3.axisBottom(xScale);
 
-d3.select("body").selectAll("div")
+const svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
+
+// d3.select("body").selectAll("div")
+//     .data(dataset)
+//     .enter()
+//     .append("div")
+//     .attr("class", "bar");
+
+    svg.selectAll("rect")
     .data(dataset)
     .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", d => (d * 10 + "px"))
+    .append("rect")
+    .attr("x", (d, i) => i* 30)
+    .attr("y", (d, i) => h - 3 * d)
+    .attr( "width" , 25 )
+    .attr( "height", (d, i) => 3 * d)
+    .attr("fill", "orange");
+    svg.append("g")
+.attr("transform", "translate(0, " + (h - padding) + ")")
+.call(xAxis);
