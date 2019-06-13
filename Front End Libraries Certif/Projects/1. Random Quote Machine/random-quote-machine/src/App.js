574853438327081ca5ca3ca83fdb8c0b817a900a
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-13 18:18:07 +0530

    Displaying All quotes from JSON

--- Front End Libraries Certif/Projects/1. Random Quote Machine/random-quote-machine/src/App.js
+++ Front End Libraries Certif/Projects/1. Random Quote Machine/random-quote-machine/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PostData from './data.json';
 import './App.css';
 
 class QuoteBoard extends React.Component{
+  
   //let quotes = ['a', 'b', 'c', 'd'];
   //var r = quotes[Math.floor(Math.random()*a.length)];
   // constructor(props){
@@ -14,9 +16,8 @@ class QuoteBoard extends React.Component{
   <h1 class="f4 bg-near-black white mv0 pv2 ph3">Title of card</h1>
   <div class="pa3 bt">
     <p class="f6 f5-ns lh-copy measure mv0">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-      vero eos et accusam et justo duo dolores et ea rebum.
+    {PostData.map((postDetail) => { 
+      return (<div><h1>{postDetail.quote} - {postDetail.author}</h1></div>)})}
     </p>
     <h6 class="fl">jsk</h6>
     <a class="f6 link dim ba ph3 pv2 mb2 dib black fr" href="#0" onClick>Button Text</a>
